a92ac759568374e7a429683c7bcd6fc45853b93b
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2024-11-04 15:01:00 +0200

    Updated roles permissions

--- server/src/constants/mongoData.js
+++ server/src/constants/mongoData.js
@@ -8,21 +8,38 @@ const {
 
 module.exports.permissions = [
   {
-    title: 'Edit users',
-    description:
-      'Edit and delete users (users are created during registration)',
-  },
-  {
-    title: 'View analytics',
-    description: 'Access to all statistics and analytics on the site',
-  },
-  {
     title: 'Change roles',
-    description: 'Change user roles',
+    description: 'Change roles for users',
+  },
+  {
+    title: 'Change permission',
+    description: 'Change permissions for roles',
+  },
+  {
+    title: 'Full view of user profiles',
+    description: 'View full information about other user profiles',
+  },
+  {
+    title: 'Limited viewing of user profiles',
+    description:
+      'View limited information about other user profiles (id, name, role, photo)',
+  },
+  {
+    title: 'Full view of own profile',
+    description: 'View full information about own profile',
+  },
+  {
+    title: 'Edit or delete own profile',
+    description: 'Edit or delete own profile information',
+  },
+  {
+    title: 'Edit or delete other users profiles',
+    description:
+      'Edit or delete information about other users (users are created during registration)',
   },
   {
     title: 'Publish content',
-    description: 'Approve or reject content publication (all types of content)',
+    description: 'Approve or reject content publication',
   },
   {
     title: 'Edit content',
@@ -33,21 +50,29 @@ module.exports.permissions = [
     description: 'Delete existing content',
   },
   {
-    title: 'Create purchase',
+    title: 'Create or update purchase',
     description:
-      'Create purchase records based on existing entities without moderation',
+      'Create or update purchase records based on existing entities without moderation',
   },
   {
     title: 'Add shops',
     description: 'Add new shops, which need to be moderated',
   },
   {
-    title: 'Edit own records',
-    description: 'Edit own records',
+    title: 'Add products',
+    description: 'Add new products, which need to be moderated',
+  },
+  {
+    title: 'Add categories',
+    description: 'Add new categories, which need to be moderated',
   },
   {
     title: 'Delete own records',
     description: 'Delete own records',
+  },
+  {
+    title: 'View analytics',
+    description: 'Access to all statistics and analytics on the site',
   },
 ];
 
@@ -55,32 +80,17 @@ module.exports.roles = async (permissionIds) => [
   {
     title: 'Administrator',
     description: 'He can do everything',
-    permissions: [
-      permissionIds['Edit users'],
-      permissionIds['View analytics'],
-      permissionIds['Change roles'],
-    ],
+    permissions: [permissionIds['View analytics']],
   },
   {
     title: 'Moderator',
     description: 'He keeps order',
-    permissions: [
-      permissionIds['Publish content'],
-      permissionIds['Edit content'],
-      permissionIds['Delete content'],
-      permissionIds['View analytics'],
-    ],
+    permissions: [permissionIds['View analytics']],
   },
   {
     title: 'User',
     description: 'He uses application',
-    permissions: [
-      permissionIds['Create purchase'],
-      permissionIds['Add shops'],
-      permissionIds['View analytics'],
-      permissionIds['Edit own records'],
-      permissionIds['Delete own records'],
-    ],
+    permissions: [permissionIds['View analytics']],
   },
 ];
 
