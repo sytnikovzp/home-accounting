0a9c410ff9c9a244e9270988daab0d28854a77fe
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2024-12-09 00:56:45 +0200

    Added seeders for DB Postgres

--- server/src/db/dbPostgres/seeders/e-products.js
+++ server/src/db/dbPostgres/seeders/e-products.js
@@ -1,8 +1,19 @@
+/* eslint-disable camelcase */
+const { Category } = require('../models');
 const { postgresData } = require('../../../constants');
 
 module.exports = {
   async up(queryInterface) {
     const { products } = await postgresData();
+    for (const product of products) {
+      const category = await Category.findOne({
+        where: { title: product.category },
+      });
+      if (category) {
+        product.category_id = category.id;
+      }
+      delete product.category;
+    }
     await queryInterface.bulkInsert('products', products, {});
   },
   async down(queryInterface) {
--- server/src/db/dbPostgres/seeders/f-purchases.js
+++ server/src/db/dbPostgres/seeders/f-purchases.js
@@ -1,8 +1,40 @@
+/* eslint-disable camelcase */
+const { Product, Shop, Measure, Currency } = require('../models');
 const { postgresData } = require('../../../constants');
 
 module.exports = {
   async up(queryInterface) {
     const { purchases } = await postgresData();
+    for (const purchase of purchases) {
+      const product = await Product.findOne({
+        where: { title: purchase.product },
+      });
+      if (product) {
+        purchase.product_id = product.id;
+      }
+      const shop = await Shop.findOne({
+        where: { title: purchase.shop },
+      });
+      if (shop) {
+        purchase.shop_id = shop.id;
+      }
+      const measure = await Measure.findOne({
+        where: { title: purchase.measure },
+      });
+      if (measure) {
+        purchase.measure_id = measure.id;
+      }
+      const currency = await Currency.findOne({
+        where: { title: purchase.currency },
+      });
+      if (currency) {
+        purchase.currency_id = currency.id;
+      }
+      delete purchase.product;
+      delete purchase.shop;
+      delete purchase.measure;
+      delete purchase.currency;
+    }
     await queryInterface.bulkInsert('purchases', purchases, {});
   },
   async down(queryInterface) {
