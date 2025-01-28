61cb60a0b98e69ca61394e26dec2e5b930742ba2
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2025-01-28 15:24:40 +0200

    Implemented RTK Query for Measures entity

---
+++ client/src/store/services/measuresApi.js
@@ -0,0 +1,80 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+import { baseQueryWithReauth } from './apiSlice';
+
+export const measuresApi = createApi({
+  reducerPath: 'measuresApi',
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ['Measure'],
+  endpoints: (builder) => ({
+    fetchAllMeasures: builder.query({
+      query: ({ page = 1, limit = 6, sort = 'uuid', order = 'asc' }) => ({
+        url: 'measures',
+        method: 'GET',
+        params: { page, limit, sort, order },
+      }),
+      transformResponse: (response, meta) => ({
+        data: response,
+        totalCount: parseInt(meta.response.headers.get('x-total-count')) || 0,
+      }),
+      providesTags: (result) => {
+        if (result?.data) {
+          return [
+            ...result.data.map(({ uuid }) => ({
+              type: 'Measure',
+              id: uuid,
+            })),
+            { type: 'Measure', id: 'LIST' },
+          ];
+        }
+        return [{ type: 'Measure', id: 'LIST' }];
+      },
+    }),
+
+    fetchMeasureByUuid: builder.query({
+      query: (measureUuid) => ({
+        url: `measures/${measureUuid}`,
+        method: 'GET',
+      }),
+      providesTags: (result, error, measureUuid) => [
+        { type: 'Measure', id: measureUuid },
+      ],
+    }),
+
+    addMeasure: builder.mutation({
+      query: ({ title, description }) => ({
+        url: 'measures',
+        method: 'POST',
+        body: { title, description },
+      }),
+      invalidatesTags: [{ type: 'Measure', id: 'LIST' }],
+    }),
+
+    editMeasure: builder.mutation({
+      query: ({ measureUuid, title, description }) => ({
+        url: `measures/${measureUuid}`,
+        method: 'PATCH',
+        body: { title, description },
+      }),
+      invalidatesTags: (result, error, { measureUuid }) => [
+        { type: 'Measure', id: measureUuid },
+      ],
+    }),
+
+    removeMeasure: builder.mutation({
+      query: (measureUuid) => ({
+        url: `measures/${measureUuid}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: [{ type: 'Measure', id: 'LIST' }],
+    }),
+  }),
+});
+
+export const {
+  useFetchAllMeasuresQuery,
+  useFetchMeasureByUuidQuery,
+  useAddMeasureMutation,
+  useEditMeasureMutation,
+  useRemoveMeasureMutation,
+} = measuresApi;
