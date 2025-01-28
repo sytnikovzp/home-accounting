eb74c54858f14b5b83107dd9131ce68a45b922d9
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2025-01-28 16:34:22 +0200

    Implemented RTK Query for Statistics entity

---
+++ client/src/store/services/statisticsApi.js
@@ -0,0 +1,39 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+import { baseQueryWithReauth } from './apiSlice';
+
+export const statisticsApi = createApi({
+  reducerPath: 'statisticsApi',
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({
+    fetchCostByCategories: builder.query({
+      query: ({ ago = 'allTime', creatorUuid }) => ({
+        url: 'statistics/categories',
+        method: 'GET',
+        params: creatorUuid ? { ago, creatorUuid } : { ago },
+      }),
+    }),
+
+    fetchCostByEstablishments: builder.query({
+      query: ({ ago = 'allTime', creatorUuid }) => ({
+        url: 'statistics/establishments',
+        method: 'GET',
+        params: creatorUuid ? { ago, creatorUuid } : { ago },
+      }),
+    }),
+
+    fetchCostByProducts: builder.query({
+      query: ({ ago = 'allTime', creatorUuid }) => ({
+        url: 'statistics/products',
+        method: 'GET',
+        params: creatorUuid ? { ago, creatorUuid } : { ago },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useFetchCostByCategoriesQuery,
+  useFetchCostByEstablishmentsQuery,
+  useFetchCostByProductsQuery,
+} = statisticsApi;
