2d09037c3069590878f27b6f23316a6ede10f26a
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2025-03-06 02:55:10 +0200

    Optimizing validationSchemes

--- client/src/utils/validationSchemes.js
+++ client/src/utils/validationSchemes.js
@@ -2,68 +2,52 @@ import * as yup from 'yup';
 
 import { parseDateString } from './sharedFunctions';
 
-const STRING_REQUIRED_SCHEME = yup
-  .string('Це поле має бути рядком')
+const STRING_SCHEME = yup
+  .string('Це має бути рядком')
   .trim('Введені дані не можуть містити пробіли на початку або в кінці')
-  .max(100, 'Введені дані не можуть перевищувати 100 символів')
-  .required('Це поле є обовʼязкове');
-
-const STRING_NULLABLE_SCHEME = yup
-  .string('Це поле має бути рядком')
-  .trim('Введені дані не можуть містити пробіли на початку або в кінці')
-  .max(100, 'Введені дані не можуть перевищувати 100 символів')
-  .nullable();
+  .max(100, 'Введені дані не можуть перевищувати 100 символів');
 
 const PASSWORD_REQUIRED_SCHEME = yup
-  .string('Це поле має бути рядком')
+  .string('Це має бути рядком')
   .trim('Введені дані не можуть містити пробіли на початку або в кінці')
   .min(8, 'Введені дані мають бути не менше 8 символів')
   .max(20, 'Введені дані не можуть перевищувати 20 символів')
   .matches(/[a-z]/, 'Пароль повинен містити хоча б одну маленьку літеру')
   .matches(/[A-Z]/, 'Пароль повинен містити хоча б одну велику літеру')
   .matches(/\d/, 'Пароль повинен містити хоча б одну цифру')
-  .required('Пароль є обовʼязковим полем');
+  .required('Будь ласка, введіть пароль');
 
 const PASSWORD_REQUIRED_CONFIRM_SCHEME = yup
-  .string('Це поле має бути рядком')
+  .string('Це має бути рядком')
   .oneOf([yup.ref('newPassword')], 'Паролі повинні співпадати')
-  .required('Підтвердження паролю є обов’язковим');
+  .required('Будь ласка, введіть підтвердження паролю');
 
-const NUMBER_REQUIRED_SCHEME = yup
-  .number()
-  .typeError('Це поле має бути числом')
-  .required('Це поле є обовʼязкове');
+const NUMBER_SCHEME = yup.number().typeError('Це має бути числом');
 
 const ARRAY_OF_STRING_NULLABLE_SCHEME = yup
-  .array('Це поле має бути масивом')
-  .of(yup.string('Це поле має бути рядком').nullable());
+  .array('Це має бути масивом')
+  .of(yup.string('Це має бути рядком').nullable());
 
 const URL_RESOURCE_NULLABLE_SCHEME = yup
-  .string('Це поле має бути рядком')
+  .string('Це має бути рядком')
   .url('Введіть коректний URL')
   .nullable();
 
-const EMAIL_REQUIRED_VALIDATION_SCHEME = yup
-  .string('Це поле має бути рядком')
-  .email('Введіть коректний e-mail')
-  .required('E-mail є обовʼязковим полем');
-
-const EMAIL_NULLABLE_VALIDATION_SCHEME = yup
-  .string('Це поле має бути рядком')
-  .email('Введіть коректний e-mail')
-  .nullable();
+const EMAIL_VALIDATION_SCHEME = yup
+  .string('Це має бути рядком')
+  .email('Введіть коректний e-mail');
 
 const STATUS_REQUIRED_SCHEME = yup
-  .string('Це поле має бути рядком')
+  .string('Це має бути рядком')
   .oneOf(['approved', 'rejected'], 'Неприпустиме значення для статусу')
-  .required('Це поле є обовʼязкове');
+  .required('Будь ласка, оберіть статус');
 
 const DATE_REQUIRED_SCHEME = yup
   .date()
   .transform(parseDateString)
   .typeError('Некоректний формат дати')
   .max(new Date(), 'Дата не може бути у майбутньому')
-  .required('Це поле є обовʼязкове');
+  .required('Будь ласка, оберіть дату');
 
 const PAGINATION_SCHEME = yup.object().shape({
   limit: yup.number().min(1).max(500).required(),
@@ -71,36 +55,36 @@ const PAGINATION_SCHEME = yup.object().shape({
 });
 
 const REGISTRATION_VALIDATION_SCHEME = yup.object().shape({
-  email: EMAIL_REQUIRED_VALIDATION_SCHEME,
-  fullName: STRING_REQUIRED_SCHEME,
+  email: EMAIL_VALIDATION_SCHEME.required('Будь ласка, введіть email'),
+  fullName: STRING_SCHEME.required('Будь ласка, введіть повне ім`я'),
   password: PASSWORD_REQUIRED_SCHEME,
 });
 
 const LOGIN_VALIDATION_SCHEME = yup.object().shape({
-  email: EMAIL_REQUIRED_VALIDATION_SCHEME,
+  email: EMAIL_VALIDATION_SCHEME.required('Будь ласка, введіть email'),
   password: PASSWORD_REQUIRED_SCHEME,
 });
 
 const FORGOT_PASSWORD_VALIDATION_SCHEME = yup.object().shape({
-  email: EMAIL_REQUIRED_VALIDATION_SCHEME,
+  email: EMAIL_VALIDATION_SCHEME.required('Будь ласка, введіть email'),
 });
 
 const PASSWORD_VALIDATION_SCHEME = yup.object().shape({
-  confirmNewPassword: PASSWORD_REQUIRED_CONFIRM_SCHEME,
   newPassword: PASSWORD_REQUIRED_SCHEME,
+  confirmNewPassword: PASSWORD_REQUIRED_CONFIRM_SCHEME,
 });
 
 const USER_VALIDATION_SCHEME = yup.object().shape({
-  email: EMAIL_NULLABLE_VALIDATION_SCHEME,
-  fullName: STRING_REQUIRED_SCHEME,
-  photo: STRING_NULLABLE_SCHEME,
-  role: STRING_NULLABLE_SCHEME,
+  email: EMAIL_VALIDATION_SCHEME.nullable(),
+  fullName: STRING_SCHEME.required('Будь ласка, введіть повне ім`я'),
+  role: STRING_SCHEME.nullable(),
+  photo: STRING_SCHEME.nullable(),
 });
 
 const ROLE_VALIDATION_SCHEME = yup.object().shape({
-  description: STRING_NULLABLE_SCHEME,
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
+  description: STRING_SCHEME.nullable(),
   permissions: ARRAY_OF_STRING_NULLABLE_SCHEME,
-  title: STRING_REQUIRED_SCHEME,
 });
 
 const MODERATION_VALIDATION_SCHEME = yup.object().shape({
@@ -108,42 +92,42 @@ const MODERATION_VALIDATION_SCHEME = yup.object().shape({
 });
 
 const EXPENSE_VALIDATION_SCHEME = yup.object().shape({
-  currency: STRING_REQUIRED_SCHEME,
+  product: STRING_SCHEME.required('Будь ласка, оберіть товар/продукт'),
+  quantity: NUMBER_SCHEME.required('Кількість обов`язкова'),
+  measure: STRING_SCHEME.required('Будь ласка, оберіть одиницю'),
+  unitPrice: NUMBER_SCHEME.required('Вартість обов`язкова'),
+  currency: STRING_SCHEME.required('Будь ласка, оберіть валюту'),
+  establishment: STRING_SCHEME.required('Будь ласка, оберіть заклад'),
   date: DATE_REQUIRED_SCHEME,
-  establishment: STRING_REQUIRED_SCHEME,
-  measure: STRING_REQUIRED_SCHEME,
-  product: STRING_REQUIRED_SCHEME,
-  quantity: NUMBER_REQUIRED_SCHEME,
-  unitPrice: NUMBER_REQUIRED_SCHEME,
 });
 
 const PRODUCT_VALIDATION_SCHEME = yup.object().shape({
-  category: STRING_NULLABLE_SCHEME,
-  title: STRING_REQUIRED_SCHEME,
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
+  category: STRING_SCHEME.nullable(),
 });
 
 const CATEGORY_VALIDATION_SCHEME = yup.object().shape({
-  title: STRING_REQUIRED_SCHEME,
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
 });
 
 const ESTABLISHMENT_VALIDATION_SCHEME = yup.object().shape({
-  description: STRING_NULLABLE_SCHEME,
-  logo: STRING_NULLABLE_SCHEME,
-  title: STRING_REQUIRED_SCHEME,
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
+  description: STRING_SCHEME.nullable(),
   url: URL_RESOURCE_NULLABLE_SCHEME,
+  logo: STRING_SCHEME.nullable(),
 });
 
 const MEASURE_VALIDATION_SCHEME = yup.object().shape({
-  description: STRING_REQUIRED_SCHEME,
-  title: STRING_REQUIRED_SCHEME,
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
+  description: STRING_SCHEME.required('Будь ласка, введіть опис'),
 });
 
 const CURRENCY_VALIDATION_SCHEME = yup.object().shape({
-  code: STRING_REQUIRED_SCHEME.matches(
+  title: STRING_SCHEME.required('Будь ласка, введіть назву'),
+  code: STRING_SCHEME.required('Будь ласка, введіть код валюти').matches(
     /^[A-Z]{3}$/,
     'Поле повинно містити рівно 3 великі латинські літери без цифр'
   ),
-  title: STRING_REQUIRED_SCHEME,
 });
 
 export {
